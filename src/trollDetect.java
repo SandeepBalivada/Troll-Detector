65f584321bf922f0dc8d221c8ae3f9c874ec168c
Sandeep Balivada <sandeepbalivada123@gmail.com>
2018-08-24 01:38:49 +0530

    trolls fourth commit

--- src/trollDetect.java
+++ src/trollDetect.java
@@ -7,17 +7,17 @@ public class trollDetect {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("This is just a demo text being used for trolls");
+		//System.out.println("This is just a demo text being used for trolls");
 		/*int words[]= new int[100];
 		words[0]=25;
 		words[1]=30;*/
 		String words[]= new String[100];
-		words[0]="hell lot of strings";
-		words[1]="another string under use";
-		//int k=1;
-		System.out.println(words[0]);
+		words[0]="fuck";
+		words[1]="douchebag";
+		int totabs=2;
+		//System.out.println(words[0]);
 		Scanner scan = new Scanner(System.in);
-		System.out.println("What do you wish to do? \n 1)Detect trolls \n 2)Update abusive words \n 3)Exit");
+		System.out.println("What do you wish to do? \n 1)Detect trolls \n 2)Display all abusive words being tracked \n 3)Update abusive words \n 4)Exit");
 		int choice = scan.nextInt();
 		if(choice==1)
 		 {
@@ -33,8 +33,28 @@ public class trollDetect {
 		 }
 		else if(choice==2)
 		{
-			
+			System.out.println("Abusive words that are being tracked are:");
+			for(int i=0;i<totabs;i++)
+				System.out.println(words[i]);
 		}
+		else if(choice==3)
+		{  
+           System.out.println("Enter the number of abusive words you wish to Update:");
+           int noabsup = scan.nextInt();
+           scan.nextLine();
+           System.out.println("Enter those "+noabsup+" abusive words:");
+           int k= totabs;
+           for(int i=0;i<noabsup;i++)
+           {
+           	words[k+i] = scan.nextLine();
+           	totabs++;
+           }
+           System.out.println("Total "+totabs+" abusive words are being tracked now.They are:");
+           for(int i=0;i<totabs;i++)
+           {
+           	System.out.println(words[i]);
+           }
+     	}
 	    scan.close();
 	    
 
